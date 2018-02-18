2472540c2d8e4d60e1a9c5a30fb098f585c1bf0d
Mateusz Urbański <matek2305@gmail.com>
2018-02-18 10:10:24 +0100

    react-redux-from example

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import React, { Component } from 'react';
-import CompetitionList from './CompetitionList';
+import React from 'react';
+import { combineReducers, createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { modelReducer, formReducer } from 'react-redux-form';
+
+import LoginFrom from './forms/login-form'
 
 
-class App extends Component {
+const store = createStore(combineReducers({
+  user: modelReducer('user'),
+  userForm: formReducer('user')
+}));
+
+class App extends React.Component {
   render() {
     return (
-      <CompetitionList />
-    );
+        <Provider store={ store }>
+          <LoginFrom/>
+        </Provider>
+    )
   }
 }
 
