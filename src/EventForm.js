5a35588aad3cc2c518bd9190af599ea147c958ca
Mateusz Urbański <matek2305@gmail.com>
2018-02-19 21:50:41 +0100

    refactored layout

--- src/EventForm.js
+++ src/EventForm.js
@@ -4,40 +4,47 @@ import { Form, FormGroup, Col, ControlLabel, FormControl } from 'react-bootstrap
 import { Button } from 'react-bootstrap';
 
 const EventForm = ({ user }) => (
-  <Form horizontal>
-    <FormGroup controlId="formHomeTeam">
-      <Col componentClass={ControlLabel} sm={4}>
-        Home team
-      </Col>
-      <Col sm={8}>
-        <FormControl type="text" placeholder="home team name" />
-      </Col>
-    </FormGroup>
+  <div style={
+    {
+      display: 'table-cell',
+      verticalAlign: 'middle'
+    }
+  }>
+    <Form horizontal style={{width: '500px'}}>
+      <FormGroup controlId="formHomeTeam">
+        <Col componentClass={ControlLabel} sm={4}>
+          Home team
+        </Col>
+        <Col sm={8}>
+          <FormControl type="text" placeholder="home team name" />
+        </Col>
+      </FormGroup>
 
-    <FormGroup controlId="formAwayTeam">
-      <Col componentClass={ControlLabel} sm={4}>
-        Away team
-      </Col>
-      <Col sm={8}>
-        <FormControl type="text" placeholder="away team name" />
-      </Col>
-    </FormGroup>
+      <FormGroup controlId="formAwayTeam">
+        <Col componentClass={ControlLabel} sm={4}>
+          Away team
+        </Col>
+        <Col sm={8}>
+          <FormControl type="text" placeholder="away team name" />
+        </Col>
+      </FormGroup>
 
-    <FormGroup controlId="formEventDate">
-      <Col componentClass={ControlLabel} sm={4}>
-        Date
-      </Col>
-      <Col sm={8}>
-        <FormControl type="date" />
-      </Col>
-    </FormGroup>
+      <FormGroup controlId="formEventDate">
+        <Col componentClass={ControlLabel} sm={4}>
+          Date
+        </Col>
+        <Col sm={8}>
+          <FormControl type="date" />
+        </Col>
+      </FormGroup>
 
-    <FormGroup>
-      <Col smOffset={4} sm={8}>
-        <Button type="submit">Add event</Button>
-      </Col>
-    </FormGroup>
-  </Form>
+      <FormGroup>
+        <Col smOffset={4} sm={8}>
+          <Button type="submit">Add event</Button>
+        </Col>
+      </FormGroup>
+    </Form>
+  </div>
 );
 
 const mapStateToProps = (state) => ({ user: state.user })
--- src/Root.js
+++ src/Root.js
@@ -1,19 +1,10 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { Grid, Row, Col } from 'react-bootstrap';
 import EventForm from './EventForm'
 
 const Root = ({ store }) => (
   <Provider store={store}>
-    <Grid>
-      <Row>
-        <Col lg={2}></Col>
-        <Col lg={6} className="align-middle">
-          <EventForm/>
-        </Col>
-        <Col lg={2}></Col>
-      </Row>
-    </Grid>
+    <EventForm/>
   </Provider>
 );
 
