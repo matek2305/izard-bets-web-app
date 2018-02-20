e7cce0df09147f5eea4989c63c09fb9a1ea01224
Mateusz Urbański <matek2305@gmail.com>
2018-02-20 17:39:11 +0100

    form margin added

--- src/EventForm.js
+++ src/EventForm.js
@@ -1,22 +1,16 @@
 import React from 'react';
-import { connect } from 'react-redux';
 import { Form, FormGroup, Col, ControlLabel, FormControl } from 'react-bootstrap';
 import { Button } from 'react-bootstrap';
 
-const EventForm = ({ user }) => (
-  <div style={
-    {
-      display: 'table-cell',
-      verticalAlign: 'middle'
-    }
-  }>
-    <Form horizontal style={{width: '500px'}}>
+const EventForm = () => (
+  <div style={{ display: 'table-cell', verticalAlign: 'middle' }}>
+    <Form horizontal style={{ width: '500px', margin: '16px' }}>
       <FormGroup controlId="formHomeTeam">
         <Col componentClass={ControlLabel} sm={4}>
           Home team
         </Col>
         <Col sm={8}>
-          <FormControl type="text" placeholder="home team name" />
+          <FormControl type="text" placeholder="home team name"/>
         </Col>
       </FormGroup>
 
@@ -25,7 +19,7 @@ const EventForm = ({ user }) => (
           Away team
         </Col>
         <Col sm={8}>
-          <FormControl type="text" placeholder="away team name" />
+          <FormControl type="text" placeholder="away team name"/>
         </Col>
       </FormGroup>
 
@@ -34,7 +28,7 @@ const EventForm = ({ user }) => (
           Date
         </Col>
         <Col sm={8}>
-          <FormControl type="date" />
+          <FormControl type="date"/>
         </Col>
       </FormGroup>
 
@@ -47,6 +41,4 @@ const EventForm = ({ user }) => (
   </div>
 );
 
-const mapStateToProps = (state) => ({ user: state.user })
-
-export default connect(mapStateToProps)(EventForm);
+export default EventForm;
