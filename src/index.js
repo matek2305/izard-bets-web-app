2dc172eca493627248fea6ad91bafe4e9f73624b
Mateusz Urbański <matek2305@gmail.com>
2018-02-27 17:45:23 +0100

    added public url prefix to routes in order to fix gh pages

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 import createHistory from 'history/createBrowserHistory';
 import Home from './home/Home';
-import EventForm from './event/view/CreateEvent';
+import CreateEvent from './event/view/CreateEvent';
 import EventCreated from './event/view/EventCreated';
 import rootReducer from './rootReducer';
 import registerServiceWorker from './registerServiceWorker';
@@ -16,7 +16,6 @@ import registerServiceWorker from './registerServiceWorker';
 import './style.css';
 
 const history = createHistory();
-
 const middlewares = [thunk, routerMiddleware(history)];
 if (process.env.NODE_ENV !== 'production') {
   middlewares.push(logger);
@@ -31,9 +30,9 @@ render(
   <Provider store={store}>
     <ConnectedRouter history={history}>
       <div className="view-container">
-        <Route exact path="/" component={Home}/>
-        <Route path="/createEvent" component={EventForm}/>
-        <Route path="/eventCreated" component={EventCreated}/>
+        <Route exact path={process.env.PUBLIC_URL + '/'} component={Home}/>
+        <Route path={process.env.PUBLIC_URL + '/createEvent'} component={CreateEvent}/>
+        <Route path={process.env.PUBLIC_URL + '/eventCreated'} component={EventCreated}/>
       </div>
     </ConnectedRouter>
   </Provider>,
