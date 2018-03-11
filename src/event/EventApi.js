bcec3b9082e1fefffbc86ff55840bddae8bed403
Mateusz Urbański <matek2305@gmail.com>
2018-03-11 19:28:21 +0100

    access mock api over https

--- src/event/EventApi.js
+++ src/event/EventApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const createEvent = (data) => {
-  return axios.post('http://demo2071198.mockable.io/events', data)
+  return axios.post('https://demo2071198.mockable.io/events', data)
     .then(response => response.data);
 }
