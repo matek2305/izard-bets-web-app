41193d9ad7484af93c3039a6f45b3dcf25a83d81
Mateusz Urbański <matek2305@gmail.com>
2018-03-26 20:34:18 +0200

    use event details component in event created view

--- src/event/view/EventCreated.js
+++ src/event/view/EventCreated.js
@@ -1,50 +1,22 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import Moment from 'react-moment';
+import EventDetails from './EventDetails';
 import { Well } from 'react-bootstrap';
 import { connect } from 'react-redux';
 import { getCreatedEvent } from '../eventReducer';
+
+import './style.css';
 
 const EventCreated = ({ event }) => {
   const eventUrl = `${window.location.origin}/events/${event.id}`;
   const joinEventUrl = `${window.location.origin}/events/${event.id}/join?invitationCode=${event.invitationCode}`;
 
   return (
-    <div style={{ textAlign: 'center' }}>
-      <div>
-        <Moment date={event.date} format="DD MMMM YYYY" /><br/>
-        <Moment date={event.date} format="HH:mm" />
+    <div>
+      <div className="created-event-details-container">
+        <EventDetails event={event} style={{margin: '32px'}} />
       </div>
-      <div style={{
-        display: 'table',
-        textAlign: 'center',
-        width: '100vw'
-      }}>
-        <span style={{
-          display: 'table-cell',
-          verticalAlign: 'middle',
-          textAlign: 'right',
-          textTransform: 'uppercase',
-          fontSize: '48px',
-          fontWeight: 'bold',
-          width: '50%'
-        }}>{event.homeTeamName}</span>
-        <span style={{
-          fontSize: '128px',
-          fontWeight: 'bold',
-          margin: '24px'
-        }}>VS</span>
-        <span style={{
-          display: 'table-cell',
-          verticalAlign: 'middle',
-          textAlign: 'left',
-          textTransform: 'uppercase',
-          fontSize: '48px',
-          fontWeight: 'bold',
-          width: '50%'
-        }}>{event.awayTeamName}</span>
-      </div>
-      <div style={{ width: '800px', margin: '0 auto' }}>
+      <div className="created-event-links-container">
         <p>Your event url (remember your password if you want to be able to update event score when it finish)</p>
         <Well bsSize="small"><a href={eventUrl}>{eventUrl}</a></Well>
         <p>Inviation url (share it if you want to invite people for this event)</p>
